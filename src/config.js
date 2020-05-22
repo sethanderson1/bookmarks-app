b8f8bb23d1e89d364b652886e7c0e48cb3152735
Seth Anderson <sdande02@gmail.com>
2020-05-22 03:45:47 -0700

    deploy to heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-  API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  // API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+  API_ENDPOINT: `https://fast-everglades-11908.herokuapp.com/`,
   // API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
